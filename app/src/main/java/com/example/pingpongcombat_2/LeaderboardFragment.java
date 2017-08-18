b1bc372d28185a2f85cfa7252c743cda718a293d
Lavitarg <sermarsel@yandex.ru>
2017-08-18 14:15:58 +0300

    new activity

---
+++ app/src/main/java/com/example/pingpongcombat_2/LeaderboardFragment.java
@@ -0,0 +1,10 @@
+package com.example.pingpongcombat_2;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Марс on 18.08.2017.
+ */
+
+public class LeaderboardFragment extends Fragment {
+}
