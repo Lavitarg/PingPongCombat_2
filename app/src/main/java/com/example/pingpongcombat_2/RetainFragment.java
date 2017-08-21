01cc30c54127df10b7d8f35a81290f6c15dc6706
Ruslanio <mirhusainow@gmaol.com>
2017-08-21 21:24:01 +0300

    Base for Retain Fragment

---
+++ app/src/main/java/com/example/pingpongcombat_2/RetainFragment.java
@@ -0,0 +1,17 @@
+package com.example.pingpongcombat_2;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Ruslanio on 21.08.2017.
+ */
+
+public class RetainFragment extends Fragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
