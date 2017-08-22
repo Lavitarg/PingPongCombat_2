54949538d86ec0155faf435a7e2fc211ce4f2c51
Ruslanio <mirhusainow@gmaol.com>
2017-08-22 19:35:05 +0300

    using savedInstanceState instead of RetainFragment

--- app/src/main/java/com/example/pingpongcombat_2/activity/RegistrationActivity.java
+++ app/src/main/java/com/example/pingpongcombat_2/activity/RegistrationActivity.java
@@ -12,36 +12,35 @@ import android.widget.EditText;
 import com.example.pingpongcombat_2.R;
 import com.example.pingpongcombat_2.RetainFragment;
 
-public class RegistrationActivity extends AppCompatActivity implements View.OnClickListener  {
+public class RegistrationActivity extends AppCompatActivity implements View.OnClickListener {
     private EditText etLoginRg;
     private EditText etPswrdRg;
     private EditText etPswrdCnRg;
     private EditText etEmailRg;
     private EditText etPincRg;
     private Button btnSUpRg;
+
     private RetainFragment retainFragment;
     private static final String RRFRAGMENT = "registrationRetainFragment";
-    @Override
 
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
 
-        etLoginRg= (EditText) findViewById(R.id.et_login_register);
-        etPswrdRg= (EditText) findViewById(R.id.et_pswrd_register);
-        etPswrdCnRg= (EditText) findViewById(R.id.et_pswrd_register);
-        etEmailRg= (EditText) findViewById(R.id.et_email_register);
-        etPincRg= (EditText) findViewById(R.id.et_pincode_register);
-        btnSUpRg= (Button) findViewById(R.id.btn_sUp_register);
+        etLoginRg = (EditText) findViewById(R.id.et_login_register);
+        etPswrdRg = (EditText) findViewById(R.id.et_pswrd_register);
+        etPswrdCnRg = (EditText) findViewById(R.id.et_pswrd_register);
+        etEmailRg = (EditText) findViewById(R.id.et_email_register);
+        etPincRg = (EditText) findViewById(R.id.et_pincode_register);
+        btnSUpRg = (Button) findViewById(R.id.btn_sUp_register);
         btnSUpRg.setOnClickListener(this);
-        FragmentManager fragmentManager= getFragmentManager();
-        retainFragment = (RetainFragment) fragmentManager.findFragmentByTag(RRFRAGMENT);
 
-        if (retainFragment == null) {
-            retainFragment = new RetainFragment();
-            fragmentManager.beginTransaction().add(retainFragment, RRFRAGMENT).commit();
+        // проеверяем, если активити создаловь первый раз (т.е. savedInstanceState == null)
+        if (savedInstanceState != null) {
+            setData(savedInstanceState);
         }
-        setData();
 
     }
 
@@ -53,28 +52,36 @@ public class RegistrationActivity extends AppCompatActivity implements View.OnCl
      */
     @Override
     public void onClick(View v) {
-        Intent intent = new Intent(RegistrationActivity.this,HomeActivity.class);
+        Intent intent = new Intent(RegistrationActivity.this, HomeActivity.class);
         startActivity(intent);
-
-
     }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        String login = etLoginRg.getText().toString();
-        String pswrd = etPswrdRg.getText().toString();
-        String cfpswrd = etPswrdCnRg.getText().toString();
-        String email = etEmailRg.getText().toString();
-        String pincode = etPincRg.getText().toString();
-        retainFragment.saveData(login,pswrd,cfpswrd,email,pincode);
-    }
-    private void setData(){
-        Bundle bundle = retainFragment.getData();
+
+    private void setData(Bundle bundle) {
         etLoginRg.setText(bundle.getString("login"));
         etPswrdRg.setText(bundle.getString("password"));
         etPswrdCnRg.setText(bundle.getString("confirmPassword"));
         etEmailRg.setText(bundle.getString("email"));
         etPincRg.setText(bundle.getString("pincode"));
     }
+
+
+    //эта байда вызовется перед уничтожением активити
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        String login = etLoginRg.getText().toString();
+        String password = etPswrdRg.getText().toString();
+        String confirmPassword = etPswrdCnRg.getText().toString();
+        String email = etEmailRg.getText().toString();
+        String pincode = etPincRg.getText().toString();
+
+        outState.putString("login", login);
+        outState.putString("password", password);
+        outState.putString("confirmPassword", confirmPassword);
+        outState.putString("email", email);
+        outState.putString("pincode", pincode);
+    }
+
 }
